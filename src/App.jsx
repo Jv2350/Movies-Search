572b13ee386b33fc76b8b5c2f03185a1be97154b
Jv2350 <tp8375735@gmail.com>
2026-01-03 12:12:34 +0530

    Fixed the import casing in App.jsx to ./pages/Favorites to match the file Favorites.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import { MovieProvider } from "./contexts/MovieContext";
 import Favorites from "./pages/Favorites";
 
+
 function App() {
   return (
     <MovieProvider>
